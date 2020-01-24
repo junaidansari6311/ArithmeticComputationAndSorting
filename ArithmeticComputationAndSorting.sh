edb116b18605768c8db2e59d6c4d3f086edb2190
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-24 12:38:19 +0530

    REFACTORED:Corrected the calculations

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -4,7 +4,8 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-operation1=`echo "scale=2;$a+$b*$c" | bc`
-operation2=`echo "scale=2;$a*$b+$c" | bc`
+operation1=`echo "$a+$b*$c" | bc`
+operation2=`echo "$a*$b+$c" | bc`
 operation3=`echo "scale=2;$c+$a/$b" | bc`
-operation4=`echo "scale=2;$(($a%$b))+$c" | bc`
+operation4=`echo "$a%$b+$c" | bc`
+
